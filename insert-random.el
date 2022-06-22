65411b27d54879eb3466733af470f56045dc385e
Lassi Kortela <lassi@lassi.io>
2022-06-22 17:48:08 +0300

    Add examples to commentary

--- insert-random.el
+++ insert-random.el
@@ -27,6 +27,21 @@
 
 ;; Use a prefix argument to say how many random characters to insert.
 
+;; The main use cases are to generate:
+
+;; - test input when writing code
+
+;; - examples of hashes, numbers, etc. when writing documentation and
+;; - specifications
+
+;; For example:
+
+;; - C-u 40 M-x insert-random-hex-lowercase gives a random git commit
+;; - hash
+
+;; - C-u 10 insert-random-alphanumeric can generate a random
+;; - 10-character password
+
 ;;; Code:
 
 (defvar insert-random-last-count 10
